def3eda6a68eae819d2e7063aa653b0c0a2dd9aa
Lynn Meza <u20201c320@upc.edu.pe>
2024-12-09 13:24:27 -0500

    update key 1

--- src/components/pokedex/PokemontList.jsx
+++ src/components/pokedex/PokemontList.jsx
@@ -3,10 +3,10 @@ import PokemonCard from './PokemonCard';
 function PokemontList({ pokemons, isFiltering }) {
 	return (
 		<>
-			{pokemons?.map((pokemon) => {
+			{pokemons?.map((pokemon, index) => {
 				const pokemonUrl = isFiltering ? pokemon.pokemon.url : pokemon.url;
 				const pokemonName = isFiltering ? pokemon.pokemon.name : pokemon.name;
-				return <PokemonCard key={pokemonName} url={pokemonUrl} />;
+				return <PokemonCard key={`${pokemonName}-${index}`} url={pokemonUrl} />;
 			})}
 		</>
 	);
